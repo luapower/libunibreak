09ce669680a427c4c0d48274975c38c56dfe8f39
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-30 16:36:15 +0000

    linux64 build

--- csrc/libunibreak/build-linux32.sh
+++ csrc/libunibreak/build-linux32.sh
@@ -1,3 +1,1 @@
 gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/linux32/libunibreak.so
-
-#cd ../.. && linux/bin/luajit libunibreak_demo.lua
--- csrc/libunibreak/build-mingw32.sh
+++ csrc/libunibreak/build-mingw32.sh
@@ -1,3 +1,1 @@
 gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/mingw32/unibreak.dll
-
-#cd ../.. && bin/luajit libunibreak_demo.lua
