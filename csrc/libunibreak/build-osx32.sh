a538fdc38f046f955695976db91ca8719c0be814
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:08:18 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/libunibreak/build-osx32.sh
+++ csrc/libunibreak/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 *.c -shared -o ../../bin/osx32/libunibreak.dylib
+gcc -arch i386 -O2 *.c -shared -install_name @loader_path/libunibreak.dylib -o ../../bin/osx32/libunibreak.dylib
--- csrc/libunibreak/build-osx64.sh
+++ csrc/libunibreak/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 *.c -shared -o ../../bin/osx64/libunibreak.dylib
+gcc -arch x86_64 -O2 *.c -shared -install_name @loader_path/libunibreak.dylib -o ../../bin/osx64/libunibreak.dylib
