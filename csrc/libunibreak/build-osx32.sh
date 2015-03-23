689dd61c6c0011275fb2d254f611df289e811f61
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:03:26 +0200

    unimportant

--- csrc/libunibreak/build-osx32.sh
+++ csrc/libunibreak/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libunibreak.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libunibreak.dylib" \
 	D=libunibreak.dylib A=libunibreak.a ./build.sh
